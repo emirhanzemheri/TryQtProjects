569cad3e2fc8df15b1d6abe20e577c4f129dc329
emirhanzemheri <emirhanzemheri9@gmail.com>
2024-01-29 19:51:03 +0300

    Tried first project and build basic ui which includes label line msgbox etc.

--- widget.cpp
+++ widget.cpp
@@ -1,6 +1,6 @@
 #include "widget.h"
 #include "./ui_widget.h"
-
+#include <QDebug>
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
@@ -12,3 +12,11 @@ Widget::~Widget()
 {
     delete ui;
 }
+
+void Widget::on_pushButton_clicked()
+{
+    qDebug()<<"Name: "<< ui->NameLine->text();
+    qDebug()<<"Id: "<< ui->IdLine->text();
+    qDebug()<<"Message: "<< ui->MessageBox->toPlainText();
+}
+
